c63c78055419ddfb3acbfad74d56923c5860f806
J <157791997+J-Nolan7@users.noreply.github.com>
2024-03-21 22:04:13 +1100

    Update index html and js to fix hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 console.log("Hi, welcome to my Portfolio site!"); 
 
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
     }
 }
 
 <i class="fa-solid fa-bars" style="color: #000000;"></i>
+
+
+
